d6e742126090e9eaf700f1b6cc7793b9d41c198b
Dhruv <s3808392@student.rmit.edu.au>
2022-01-25 16:29:48 +1100

    Dom router added

--- frontend/firefly-react/src/App.js
+++ frontend/firefly-react/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import CreateTask from "./Components/CreateTask";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <CreateTask />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={CreateTask} />
+      </div>
+    </Router>
   );
 }
 
